c6308d8eea067601d05bc46caa20f2330d98b1ab
Carlos Bordalo <carlosvitor6@hotmail.com>
2020-01-28 02:39:35 -0300

    Add CRUD

--- index.js
+++ index.js
@@ -2,10 +2,49 @@ const express = require('express');
 
 const server = express();
 
-server.get('/users/:id', (req, res) => {
-const { id } = req.params;
+// tells express to read json from req.body
+server.use(express.json());
 
-  return res.json({ message: `Buscando o usuário ${id}` });
+const users = ['Diego', 'Robson', 'Victor'];
+
+// lists all users
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
+
+// returns users index
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
+});
+
+// creates user
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+// edit user
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+// delete user
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
 });
 
 server.listen(3000);
